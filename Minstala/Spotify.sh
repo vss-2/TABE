35a93c4e45deb1e31fe5d081d21a37d6e97af12b
Vitor Sousa Silva <vss2@ug3c28.windows.cin.ufpe.br>
2019-12-20 15:28:55 -0300

    A funcao de criar icone esta funcionando, projeto concluido

--- Minstala/Spotify.sh
+++ Minstala/Spotify.sh
@@ -6,9 +6,9 @@ python3 Spotify.py
 mv Spotify.deb Spotify
 mv versions.txt Spotify
 cd Spotify
+localSpotify=$(pwd)
 ar x Spotify.deb
 tar xvzf data.tar.gz
-$localSpotify=$(pwd)
 cd usr
 cd bin
 mv $(pwd)/spotify $(pwd)/Spotify
@@ -18,25 +18,25 @@ mv $(pwd)/spotify $(pwd)/Spotify
 ################################# Criando Atalho no Desktop ########################################
 
 # Criando atalho
-cd ~/Desktop
 
-echo '
-[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=false
-Exec=${localSpotify}/usr/bin/Spotify
-Name=Spotify
-Comment=Spotify
-Icon=${!localSpotify}/share/spotify/icons/spotify-linux-512.png
-' >> Spotify.desktop
+echo ${localSpotify}
+echo "[Desktop Entry]" >> Spotify.desktop
+echo "Version=1.0" >> Spotify.desktop
+echo "Type=Application" >> Spotify.desktop
+echo "Terminal=false" >> Spotify.desktop
+execPlace="Exec=${localSpotify}/usr/bin/Spotify"
+echo ${execPlace} >> Spotify.desktop
+echo "Name=Spotify" >> Spotify.desktop
+echo "Comment=Spotify" >> Spotify.desktop
+iconPlace="Icon=${localSpotify}/usr/share/spotify/icons/spotify-linux-512.png"
+echo ${iconPlace} >> Spotify.desktop
+
 chmod +x Spotify.desktop
+mv Spotify.desktop ~/Desktop
 
 # grep _amd64.deb, pega as linhas que tem _amd64.deb
 # wc -l lê a quantidade de linhas
 # sed 'Xd' arquivo, deleta as X linhas do arquivo
 
-:'
-curl -s http://packages.linuxmint.com/pool/import/s/spotify-client/ | grep _amd64.deb > ok.txt | wc -l ok.txt > lines.txt  | sed '$d' ok.txt
-wc -l ok.txt
-'
+# curl -s http://packages.linuxmint.com/pool/import/s/spotify-client/ | grep _amd64.deb > ok.txt | wc -l ok.txt > lines.txt  | sed '$d' ok.txt
+# wc -l ok.txt
