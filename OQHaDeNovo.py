59b1a2803e35f194018f0a64e672418f75ff513c
Vitor Sousa <vss2@cin.ufpe.br>
2020-03-11 22:14:50 -0300

    Implementada da ideia geral do que ha de novo
    O esqueleto deve estar mais ou menos funcional

--- OQHaDeNovo.py
+++ OQHaDeNovo.py
@@ -1,18 +1,38 @@
+# -*- coding: utf-8 -*-
+
 import csv
 from subprocess import call 
 
-def tratarString(titulo, autor):
-        return
-
 if __name__ == "__main__":
-        call(['curl', 'https://spotifycharts.com/regional/br/weekly/latest/download', '>>', 'arquivo.csv'])
-        with open('arquivo.csv') as chartscsv:
+        print('Escolha a semana 1, depois a semana 2')
+        call(['curl', 'https://spotifycharts.com/regional/br/weekly/latest/download', '>>', 'semana1.csv'])
+        call(['curl', 'https://spotifycharts.com/regional/br/weekly/latest/download', '>>', 'semana2.csv'])
+
+        hashmap = {'musica': 'aparições'}
+        novasmus = []
+        with open('semana1.csv') as chartscsv:
                 leitor_csv = csv.reader(chartscsv, delimiter=',')
                 contador = 0
-                for coluna in csv_reader:
+                for row in csv_reader:
                         if contador == 0:
                                 continue
-                        else: 
-                                titulo = row[1]
-                                autor = row[2]
-                                tratarString(titulo, autor)
+                        else:                         
+                                # Retira acentos, cedilhas e tudo que não for ascii-safe
+                                titulo1 = bytes(row[1], 'utf-8').decode('ascii','ignore')
+                                autor1  = bytes(row[2], 'utf-8').decode('ascii','ignore')
+                                musica = titulo1 + ' de ' + autor1
+                                hashmap[musica] = 1
+        with open('semana2.csv') as chartscsv:
+                leitor_csv = csv.reader(chartscsv, delimiter=',')
+                contador = 0
+                for row in csv_reader:
+                        if contador == 0:
+                                continue
+                        else:
+                                titulo1 = bytes(row[1], 'utf-8').decode('ascii','ignore')
+                                autor1  = bytes(row[2], 'utf-8').decode('ascii','ignore')
+                                musica = titulo1 + ' de ' + autor1
+                                hashmap[musica] = hashmap[musica] + 1
+                                if(hashmap[musica] > 1):
+                                        novasmus.append(musica)
+        print(novamus)
