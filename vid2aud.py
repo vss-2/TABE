628f39e33305b9762fc6b3f9bba5aaf5d76ded88
vss-2 <32624576+vss-2@users.noreply.github.com>
2020-05-27 00:05:34 -0300

    Update vid2aud.py

--- vid2aud.py
+++ vid2aud.py
@@ -1,11 +1,15 @@
 from subprocess import call
 
 if __name__ == "__main__":
+        print('Se qualquer coisa der errado, verifique se há: ffmpeg E youtube-dl, ambos atualizados')
         link = input('Insira o link:\n')
-        linkID = ''
+        linkID, link_arg = '', ''
         if(link[link.index('y')+5] == '.'):
                 # Link encurtado
                 linkID = link[link.index('y')+8]
                 link_arg = 'https://youtube.com/watch?v=' + linkID
+        elif(len(link) > 43):
+                # Provável link padrão de playlist ou com timestamp
+                link_arg = link[:43]
         call(['youtube-dl', link_arg, '-x', '--audio-extract', '\"m4a\"', '-o', '\"%(title)s.%(ext)\"'])
         return
