d5ccc389538c1dc920ef65ad5926179cccc2c3fb
vss-2 <32624576+vss-2@users.noreply.github.com>
2020-05-29 21:07:51 -0300

    Update vid2aud.py

--- vid2aud.py
+++ vid2aud.py
@@ -1,15 +1,40 @@
 from subprocess import call
 
+def cortador_ID(idk):
+        if(idk.startswith('youtube.com/watch?v=')):
+                idk = idk[20:31]
+        elif(idk.startswith('youtu.be/')):
+                idk = idk[9:20]
+        return idk
+
+def identificador(url):
+        # Cada elif remove da string o começo
+        if(url.startswith('http://www.')):
+                url = url[11:]
+                return cortador_ID(url)
+        elif(url.startswith('https://www.')):
+                url = url[12:]
+                return cortador_ID(url)
+        elif(url.startswith('www.')):
+                url = url[4:]
+                return cortador_ID(url)
+        elif(url.startswith('youtube.com/watch?v='):
+                # Provavelmente já tá no formato youtube.com/ID
+                return url[20:31]
+	else:
+                print('ID não identificado, encerrando!')
+                return exit()
+
+
 if __name__ == "__main__":
-        print('Se qualquer coisa der errado, verifique se há: ffmpeg E youtube-dl, ambos atualizados')
+        print('Se qualquer coisa der errado, verifique se há: ffmpeg E youtube-dl, ambos atualizados!')
         link = input('Insira o link:\n')
-        linkID, link_arg = '', ''
-        if(link[link.index('y')+5] == '.'):
-                # Link encurtado
-                linkID = link[link.index('y')+8]
-                link_arg = 'https://youtube.com/watch?v=' + linkID
-        elif(len(link) > 43):
-                # Provável link padrão de playlist ou com timestamp
-                link_arg = link[:43]
-        call(['youtube-dl', link_arg, '-x', '--audio-extract', '\"m4a\"', '-o', '\"%(title)s.%(ext)\"'])
-        return
+        link_arg = identificador(link)
+        if(link_arg == ''):
+                print('Link vazio')
+                exit()
+        else:
+                print('Iniciando download de: ')
+                call(['youtube-dl', '-e', link_arg])
+                call(['youtube-dl', '-x', '--audio-format', str("m4a"), '-o', str("%(title)s.%(ext)s"), link_arg])
+        exit()
