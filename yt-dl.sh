3bb187ac35f0bdf292b3773dba1a007f879d7cdd
vss-2 <32624576+vss-2@users.noreply.github.com>
2020-06-22 11:38:46 -0300

    Sintax fix

--- yt-dl.sh
+++ yt-dl.sh
@@ -1,6 +1,6 @@
-[if $2 eq "pc"]
+if [ $2 = "pc" ]
 then
-	youtube-dl $1 -x --audio-format "m4a" --audio-quality 320K -o "~/Músicas/%(title)s.%(ext)s"
+	youtube-dl $1 -x --audio-format "m4a" --audio-quality 320K -o "~/Música/%(title)s.%(ext)s"
 else
 	youtube-dl $1 -x --audio-format "m4a" --audio-quality 320K -o "~/storage/shared/Music/%(title)s.%(ext)s"
 fi
